cc33b95ab51a425225d2ac467ce537b5bb83bdbb
Gabriel D <gabrieldavidfs@hotmail.com>
2023-03-06 17:38:05 +0100

    Imput ahora filtra letras

--- src/components/Aportacion.jsx
+++ src/components/Aportacion.jsx
@@ -7,42 +7,36 @@ function Aportacion({aPagar}) {
 
     let [cantidadAportada, setCantidadAportada] = useState(0);
     let [valorSaldo, setValorSaldo] = useState(0);
-    /*let [pagoIntroducido, setPagoIntroducido] = useState(0);*/
+    let [textoAlmacenado, setTextoAlmacenado] = useState(0);
 
     function calcularSaldo(){
-        setValorSaldo (cantidadAportada - aPagar);
+        setValorSaldo (parseFloat(cantidadAportada) - aPagar);
     };
 
+
     function manejadorIntroducirPago(event){
-    
-    let textoIntroducido = event.target.value
-    /*if (/^[0-9]*\.?[0-9]{0,2}$/.test(textoIntroducido) === true){
-        setPagoIntroducido (textoIntroducido);
-        console.log("Pago introducido en el else", pagoIntroducido)
-          setCantidadAportada(parseFloat(pagoIntroducido));
-    } else {
-        console.log("letras!!")
-            event.target.value = pagoIntroducido;
-    };*/
 
-        let pagoIntroducido = parseFloat(event.target.value);
-        console.log("Pago introducido antes del if", pagoIntroducido)
-        if (isNaN(pagoIntroducido) === true)
-         {
-            console.log("Pago introducido en el if", pagoIntroducido)
-            event.target.value = "";
-            pagoIntroducido = 0;
-        } else 
-        console.log("Pago introducido en el else", pagoIntroducido)
-          setCantidadAportada(pagoIntroducido);
-      };
+        let textoIntroducido = event.target.value
 
-      function manejadorBotonConfirmar(){
-        calcularSaldo();
-          console.log("precio persona", aPagar)
-          console.log("lo que paga", cantidadAportada)
-          console.log("saldo disponible", valorSaldo)
-      };
+        if (/^[0-9]*\.?[0-9]{0,2}$/.test(textoIntroducido) === true){
+            setTextoAlmacenado(textoIntroducido);
+            setCantidadAportada(textoIntroducido);
+            console.log("Texto introducido", textoIntroducido)
+            console.log("Pago introducido", textoAlmacenado)
+            
+        } else {
+            console.log("Letras", textoAlmacenado)
+                event.target.value = textoAlmacenado;
+            };  
+    };
+
+
+    function manejadorBotonConfirmar(){
+    calcularSaldo();
+        console.log("precio persona", aPagar)
+        console.log("lo que paga", cantidadAportada)
+        console.log("saldo disponible", valorSaldo)
+    };
 
 
     return (
