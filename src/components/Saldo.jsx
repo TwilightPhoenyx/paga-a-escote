01e7298e0163976af222571b0febd7173d763902
Gabriel D <gabrieldavidfs@hotmail.com>
2023-03-08 16:34:22 +0100

    Saldo

--- src/components/Saldo.jsx
+++ src/components/Saldo.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import SmileHappy from "../images/SmileHappy.png"
+import SmileNerveus from "../images/SmileNerveus.png"
+import SmileWorry from "../images/SmileWorry.png"
+
+
 function Saldo ({saldo}) {
 
-    console.log (saldo);
+    let [imagen, setImagen] = useState();
 
-    return(
-        <p>Saldo {saldo.toFixed(2)}</p>
+   useEffect(() => {
+
+   if (saldo === 0) {setImagen(SmileHappy)} // El smile ha pagado
+   else if (saldo >12) {setImagen(SmileNerveus)}  // El smile recibe el pago
+   else {setImagen(SmileWorry)}  // El smile tiene que pagar
+
+   
+
+   
+
+   }
+   )
+   
+   return(
+        <div>
+            <p>Saldo {saldo.toFixed(2)}</p>
+
+           
+            <img src={imagen} />
+        </div>
     );
     
 };
